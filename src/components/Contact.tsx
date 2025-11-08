07a041bf1acd87a92443fb59804a9148e3ba2cf9
juanchacon98 <95106627+juanchacon98@users.noreply.github.com>
2025-11-08 09:03:09 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -29,7 +29,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch('/api/send-mail', {
+      const response = await fetch('https://mail.rv2ven.com/send-mail', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
