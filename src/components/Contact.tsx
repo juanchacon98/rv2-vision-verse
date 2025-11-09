9def5d8c8895f4d8a75da5a57c82e1f42c3be1cc
juanchacon98 <95106627+juanchacon98@users.noreply.github.com>
2025-11-09 14:07:23 -0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -30,6 +30,7 @@ if (formData.telefono.length > 13) {
 
     try {
       const response = await fetch('/api/send-mail', {
+
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
